f71eebe429f2dd9422cc2e37cc713cb37aa017eb
Invegorant <kraisyvirus@yandex.ru>
2025-10-02 12:31:43 +0700

    added rococo-museum

---
+++ rococo-museum/src/main/resources/db/migration/rococo-museum/V1__create_museum.sql
@@ -0,0 +1,11 @@
+create table if not exists museum
+(
+    id                     binary(16)    unique not null default (UUID_TO_BIN(UUID(), true)),
+    title                  varchar(255)  not null,
+    description            text          not null,
+    city                   varchar(255)  not null,
+    geo_id                 binary(16)    not null,
+    photo                  longblob      not null,
+    primary key (id),
+    constraint fk_country_id foreign key (geo_id) references `country` (id)
+);

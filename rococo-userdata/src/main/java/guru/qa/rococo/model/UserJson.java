6c759511ce08dc3ce39c3fc691bdcb06053c4f98
Invegorant <kraisyvirus@yandex.ru>
2025-10-02 12:31:27 +0700

    added rococo-userdata

---
+++ rococo-userdata/src/main/java/guru/qa/rococo/model/UserJson.java
@@ -0,0 +1,11 @@
+package guru.qa.rococo.model;
+
+import java.util.UUID;
+
+public record UserJson(
+        UUID id,
+        String username,
+        String firstname,
+        String lastname,
+        String avatar) {
+}

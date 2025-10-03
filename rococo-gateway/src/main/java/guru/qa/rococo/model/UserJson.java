07ac62f151b588335ed73846af39696324e923ee
Invegorant <kraisyvirus@yandex.ru>
2025-10-03 13:01:41 +0700

    added rococo-gateway

---
+++ rococo-gateway/src/main/java/guru/qa/rococo/model/UserJson.java
@@ -0,0 +1,32 @@
+package guru.qa.rococo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import guru.qa.grpc.rococo.grpc.UserResponse;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
+public record UserJson(
+        @JsonProperty("id")
+        UUID id,
+        @JsonProperty("username")
+        String username,
+        @JsonProperty("firstname")
+        @Size(max = 30, message = "username can not be longer than 30 symbols")
+        String firstname,
+        @JsonProperty("lastname")
+        @Size(max = 50, message = "lastname can not be longer than 30 symbols")
+        String lastname,
+        @JsonProperty("avatar")
+        String avatar
+) {
+    public static UserJson fromGrpcMessage(UserResponse response) {
+        return new UserJson(
+                UUID.fromString(response.getId()),
+                response.getUsername(),
+                response.getFirstname(),
+                response.getLastname(),
+                response.getAvatar().toStringUtf8()
+        );
+    }
+}
